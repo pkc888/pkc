afd4f730e03e518112be592c5c9eb7ca259ea299
pkc888 <44221142+pkc888@users.noreply.github.com>
2018-10-30 16:50:40 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+n1=float(input("enter any number"))
+n2=float(input("enter any number"))
+def add(n1,n2):
+    return(n1+n2)
+def sub(n1,n2):
+    return(n1-n2)
+def mul(n1,n2):
+    return(n1*n2)
+def div(n1,n2):
+    return(n1/n2)
+print("addition is",add(n1,n2))
+print("subtraction is",sub(n1,n2))
+print("multiplition is",mul(n1,n2))
+print("division is",div(n1,n2))
